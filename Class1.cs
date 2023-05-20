4ea03c3e0ffe0154ff6d4ad7a6dcebe3e1d1ed26
Dreon207 <114486506+Dreon207@users.noreply.github.com>
2023-05-20 15:52:19 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+namespace DesSezLibrary
+{
+    public class Car
+    {
+        public string MachineBrand { get; set; }
+        public string Model { get; set; }
+    }
+    public class Deserialization_Serialization
+    {
+        public static T S <T>(T carM1)
+        {
+            string json1 = JsonConvert.SerializeObject(carM1);
+            File.WriteAllText("C:\\Temp\\NewJS.json", json1);
+            return carM1;
+        }
+        public static T D <T>(T carM1)
+        {
+           string json = File.ReadAllText("C:\\Temp\\NewJS.json");
+            T itogM1 = JsonConvert.DeserializeObject<T>(json);
+            return itogM1;
+        }
+    }
+}
